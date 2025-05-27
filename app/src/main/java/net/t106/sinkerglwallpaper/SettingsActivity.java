54129fc10d091fb8c189631d85183460a852432e
106- <segmentation-fault@yandex.com>
2025-05-27 11:58:25 +0900

    修正

--- app/src/main/java/net/t106/sinkerglwallpaper/SettingsActivity.java
+++ app/src/main/java/net/t106/sinkerglwallpaper/SettingsActivity.java
@@ -23,5 +23,81 @@ public class SettingsActivity extends AppCompatActivity {
 		public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
 			setPreferencesFromResource(R.xml.pref, rootKey);
 		}
+		
+		@Override
+		public void onDisplayPreferenceDialog(androidx.preference.Preference preference) {
+			if (preference instanceof TextBox_Pref) {
+				androidx.preference.PreferenceDialogFragmentCompat dialogFragment = 
+					new TextBoxPreferenceDialogFragmentCompat();
+				Bundle bundle = new Bundle(1);
+				bundle.putString("key", preference.getKey());
+				dialogFragment.setArguments(bundle);
+				dialogFragment.setTargetFragment(this, 0);
+				dialogFragment.show(getParentFragmentManager(), "androidx.preference.PreferenceFragment.DIALOG");
+			} else if (preference instanceof seekbar_pref) {
+				androidx.preference.PreferenceDialogFragmentCompat dialogFragment = 
+					new SeekBarPreferenceDialogFragmentCompat();
+				Bundle bundle = new Bundle(1);
+				bundle.putString("key", preference.getKey());
+				dialogFragment.setArguments(bundle);
+				dialogFragment.setTargetFragment(this, 0);
+				dialogFragment.show(getParentFragmentManager(), "androidx.preference.PreferenceFragment.DIALOG");
+			} else if (preference instanceof size_change_pref) {
+				androidx.preference.PreferenceDialogFragmentCompat dialogFragment = 
+					new SizeChangePreferenceDialogFragmentCompat();
+				Bundle bundle = new Bundle(1);
+				bundle.putString("key", preference.getKey());
+				dialogFragment.setArguments(bundle);
+				dialogFragment.setTargetFragment(this, 0);
+				dialogFragment.show(getParentFragmentManager(), "androidx.preference.PreferenceFragment.DIALOG");
+			} else {
+				super.onDisplayPreferenceDialog(preference);
+			}
+		}
+	}
+	
+	public static class TextBoxPreferenceDialogFragmentCompat extends androidx.preference.PreferenceDialogFragmentCompat {
+		@Override
+		protected void onBindDialogView(android.view.View view) {
+			super.onBindDialogView(view);
+			TextBox_Pref preference = (TextBox_Pref) getPreference();
+			preference.onBindDialogView(view);
+		}
+		
+		@Override
+		public void onDialogClosed(boolean positiveResult) {
+			TextBox_Pref preference = (TextBox_Pref) getPreference();
+			preference.onDialogClosed(positiveResult);
+		}
+	}
+	
+	public static class SeekBarPreferenceDialogFragmentCompat extends androidx.preference.PreferenceDialogFragmentCompat {
+		@Override
+		protected void onBindDialogView(android.view.View view) {
+			super.onBindDialogView(view);
+			seekbar_pref preference = (seekbar_pref) getPreference();
+			preference.onBindDialogView(view);
+		}
+		
+		@Override
+		public void onDialogClosed(boolean positiveResult) {
+			seekbar_pref preference = (seekbar_pref) getPreference();
+			preference.onDialogClosed(positiveResult);
+		}
+	}
+	
+	public static class SizeChangePreferenceDialogFragmentCompat extends androidx.preference.PreferenceDialogFragmentCompat {
+		@Override
+		protected void onBindDialogView(android.view.View view) {
+			super.onBindDialogView(view);
+			size_change_pref preference = (size_change_pref) getPreference();
+			preference.onBindDialogView(view);
+		}
+		
+		@Override
+		public void onDialogClosed(boolean positiveResult) {
+			size_change_pref preference = (size_change_pref) getPreference();
+			preference.onDialogClosed(positiveResult);
+		}
 	}
 }
