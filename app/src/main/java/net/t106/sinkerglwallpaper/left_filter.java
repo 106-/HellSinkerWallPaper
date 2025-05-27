aa8cc04ba4703372ea6acd955377ec073ef65f61
106- <segmentation-fault@yandex.com>
2025-05-28 03:45:13 +0900

    phase3

--- app/src/main/java/net/t106/sinkerglwallpaper/left_filter.java
+++ app/src/main/java/net/t106/sinkerglwallpaper/left_filter.java
@@ -1,55 +1,114 @@
 package net.t106.sinkerglwallpaper;
-import javax.microedition.khronos.opengles.GL10;
+import android.opengl.GLES32;
 
+/**
+ * Left filter for OpenGL ES 3.2
+ * Renders a customizable colored overlay with user-selectable blend modes
+ */
 public class left_filter extends graveyard{
+
+	private boolean isSmallSize = false;
 
 	public left_filter()
 	{
 		super();
+		// Center overlay covering most of the screen
 		apex = new float[] { -0.7f, -2.5f, 0.7f, -2.5f, -0.7f, 2.5f, 0.7f, 2.5f, };
+		// No texture coordinates needed for color-only rendering
+		coords = new float[] { 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f };
+		
+		// Keep legacy buffer creation for compatibility
 		ab = SinkerService.makeFloatBuffer(apex);
+		cb = SinkerService.makeFloatBuffer(coords);
 	}
 	
 	@Override
-	public void Draw(GL10 gl) {
+	protected void createShaderProgram() {
+		// Use blend shader program for customizable blending
+		shaderProgram = ShaderLoader.Programs.createBlendProgram(SinkerService.getContext());
+	}
+	
+	@Override
+	public void Draw(float[] viewMatrix, float[] projectionMatrix) {
+		// Update MVP matrix (no rotation, just basic transformation)
+		updateMVP(viewMatrix, projectionMatrix);
+		
+		// Bind shader and set uniforms
+		bindShader();
+		
+		// No texture binding needed for color-only rendering
+		ShaderUtils.setUniform1i(textureLocation, 0);
+		
+		// Set blend mode from user settings
+		ShaderUtils.setUniform1i(blendModeLocation, SinkerService.blend_type);
+		
+		// Set color from user settings (convert from 0-100 range to 0.0-1.0)
 		int[] col = SinkerService.col;
-		gl.glColor4f((float)(col[0]/100.0),(float)(col[1]/100.0),(float)(col[2]/100.0),(float)(col[3]/100.0));
-		//頂点を有効化
-		gl.glVertexPointer(2, GL10.GL_FLOAT, 0, ab);
-		gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+		float red = col[0] / 100.0f;
+		float green = col[1] / 100.0f;
+		float blue = col[2] / 100.0f;
+		float alpha = col[3] / 100.0f;
+		ShaderUtils.setUniform4f(colorLocation, red, green, blue, alpha);
+		
+		// Enable blending with mode based on user settings
+		GLES32.glEnable(GLES32.GL_BLEND);
 		
 		switch(SinkerService.blend_type)
 		{
-		//加算
-		case 0:gl.glBlendFunc(GL10.GL_ONE, GL10.GL_ONE);break;
-		//乗算
-		case 1:gl.glBlendFunc(GL10.GL_ZERO, GL10.GL_SRC_COLOR);break;
-		//アルファ
-		case 2:gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE);break;
-		//排他的論理和
-		case 3:gl.glBlendFunc(GL10.GL_ONE_MINUS_DST_COLOR, GL10.GL_ONE_MINUS_SRC_COLOR);break;
+		// Additive
+		case 0:
+			GLES32.glBlendFunc(GLES32.GL_ONE, GLES32.GL_ONE);
+			break;
+		// Multiplicative  
+		case 1:
+			GLES32.glBlendFunc(GLES32.GL_ZERO, GLES32.GL_SRC_COLOR);
+			break;
+		// Alpha
+		case 2:
+			GLES32.glBlendFunc(GLES32.GL_SRC_ALPHA, GLES32.GL_ONE);
+			break;
+		// XOR (Exclusive OR)
+		case 3:
+			GLES32.glBlendFunc(GLES32.GL_ONE_MINUS_DST_COLOR, GLES32.GL_ONE_MINUS_SRC_COLOR);
+			break;
+		default:
+			GLES32.glBlendFunc(GLES32.GL_SRC_ALPHA, GLES32.GL_ONE_MINUS_SRC_ALPHA);
+			break;
 		}
 		
-		gl.glEnable(GL10.GL_BLEND);
-		//頂点の描画
-		gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, 4);
-		//無効化
-	   	gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
-		gl.glDisable(GL10.GL_BLEND);
-	   	gl.glColor4f(1, 1, 1, 1);
+		// Bind VAO and draw
+		BufferUtils.bindVAO(vao);
+		BufferUtils.drawQuad();
+		BufferUtils.unbindVAO();
+		
+		// Disable blending
+		GLES32.glDisable(GLES32.GL_BLEND);
 	}
 
 	@Override
-	public void Update(GL10 gl) {}
+	public void Update(float deltaTime) {
+		// No animation needed for static filter
+	}
 	
 	@Override
 	public void sizechange(boolean smollflg)
 	{
+		isSmallSize = smollflg;
+		
+		// Update vertex data based on size
 		if(smollflg) {
-			ab = SinkerService.makeFloatBuffer(new float[] { -0.5f, -1.5f, 0.5f, -1.5f, -0.5f, 1.5f, 0.5f, 1.5f, });
+			apex = new float[] { -0.5f, -1.5f, 0.5f, -1.5f, -0.5f, 1.5f, 0.5f, 1.5f, };
 		} else {
-			ab = SinkerService.makeFloatBuffer(new float[] { -0.7f, -1.5f, 0.7f, -1.5f, -0.7f, 1.5f, 0.7f, 1.5f, });
+			apex = new float[] { -0.7f, -1.5f, 0.7f, -1.5f, -0.7f, 1.5f, 0.7f, 1.5f, };
+		}
+		
+		// Update buffer
+		ab = SinkerService.makeFloatBuffer(apex);
+		
+		// Recreate VAO with new vertex data
+		if (vao != 0) {
+			BufferUtils.deleteVAO(vao);
+			createBuffers();
 		}
 	}
-
 }
--- app/src/main/java/net/t106/sinkerglwallpaper/right_filter.java
+++ app/src/main/java/net/t106/sinkerglwallpaper/right_filter.java
@@ -1,48 +1,93 @@
 package net.t106.sinkerglwallpaper;
-import javax.microedition.khronos.opengles.GL10;
+import android.opengl.GLES32;
 
+/**
+ * Right side filter for OpenGL ES 3.2
+ * Renders a vertical colored strip on the right side with invert blend mode
+ */
 public class right_filter extends graveyard {
+
+	private boolean isSmallSize = false;
+	
+	// Filter color (pinkish)
+	private static final float RED = 1.0f;
+	private static final float GREEN = 0.5f;
+	private static final float BLUE = 0.5f;
+	private static final float ALPHA = 0.5f;
 
 	public right_filter()
 	{
 		super();
+		// Right side vertical strip (default size)
 		apex = new float[] { 0f, -2.5f, 0.7f, -2.5f, 0f, 2.5f, 0.7f, 2.5f, };
+		// No texture coordinates needed for color-only rendering
+		coords = new float[] { 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f };
+		
+		// Keep legacy buffer creation for compatibility
 		ab = SinkerService.makeFloatBuffer(apex);
+		cb = SinkerService.makeFloatBuffer(coords);
 	}
 	
 	@Override
-	public void Draw(GL10 gl) {
-		//gl.glColor4f(1f,0.554f,0.18f,0.5f);
-		gl.glColor4f(1f,0.5f,0.5f,0.5f);
-		//頂点を有効化
-		gl.glVertexPointer(2, GL10.GL_FLOAT, 0, ab);
-		gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
-		//OpenGLを2Dモードに
-		//gl.glEnable(GL10.GL_TEXTURE_2D);
-		//反転を有効にする
-		gl.glBlendFunc(GL10.GL_ONE_MINUS_DST_COLOR, GL10.GL_ZERO);
-		//gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE);
-		gl.glEnable(GL10.GL_BLEND);
-		//頂点の描画
-		gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, 4);
-		//無効化
-	   	//gl.glDisable(GL10.GL_TEXTURE_2D);
-	   	gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
-		gl.glDisable(GL10.GL_BLEND);
-	   	gl.glColor4f(1, 1, 1, 1);
+	protected void createShaderProgram() {
+		// Use basic shader program without texture
+		shaderProgram = ShaderLoader.Programs.createBasicProgram(SinkerService.getContext());
+	}
+	
+	@Override
+	public void Draw(float[] viewMatrix, float[] projectionMatrix) {
+		// Update MVP matrix (no rotation, just basic transformation)
+		updateMVP(viewMatrix, projectionMatrix);
+		
+		// Bind shader and set uniforms
+		bindShader();
+		
+		// No texture binding needed for color-only rendering
+		ShaderUtils.setUniform1i(textureLocation, 0);
+		
+		// Set blend mode to custom invert effect (can be simulated with XOR mode)
+		ShaderUtils.setUniform1i(blendModeLocation, 3); // XOR mode
+		
+		// Set filter color (pinkish)
+		ShaderUtils.setUniform4f(colorLocation, RED, GREEN, BLUE, ALPHA);
+		
+		// Enable special blending for invert effect
+		GLES32.glEnable(GLES32.GL_BLEND);
+		GLES32.glBlendFunc(GLES32.GL_ONE_MINUS_DST_COLOR, GLES32.GL_ZERO);
+		
+		// Bind VAO and draw
+		BufferUtils.bindVAO(vao);
+		BufferUtils.drawQuad();
+		BufferUtils.unbindVAO();
+		
+		// Disable blending
+		GLES32.glDisable(GLES32.GL_BLEND);
 	}
 
 	@Override
-	public void Update(GL10 gl) {}
+	public void Update(float deltaTime) {
+		// No animation needed for static filter
+	}
 	
 	@Override
 	public void sizechange(boolean smollflg)
 	{
+		isSmallSize = smollflg;
+		
+		// Update vertex data based on size
 		if(smollflg) {
-			ab = SinkerService.makeFloatBuffer(new float[] { 0f, -1.5f, 0.5f, -1.5f, 0f, 1.5f, 0.5f, 1.5f, });
+			apex = new float[] { 0f, -1.5f, 0.5f, -1.5f, 0f, 1.5f, 0.5f, 1.5f, };
 		} else {
-			ab = SinkerService.makeFloatBuffer(new float[] { 0f, -1.5f, 0.7f, -1.5f, 0f, 1.5f, 0.7f, 1.5f, });
+			apex = new float[] { 0f, -1.5f, 0.7f, -1.5f, 0f, 1.5f, 0.7f, 1.5f, };
+		}
+		
+		// Update buffer
+		ab = SinkerService.makeFloatBuffer(apex);
+		
+		// Recreate VAO with new vertex data
+		if (vao != 0) {
+			BufferUtils.deleteVAO(vao);
+			createBuffers();
 		}
 	}
-
 }
